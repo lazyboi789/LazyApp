87931512376ad5c5c5fa60d5f05a93b74184a092
steven lu <steven lu>
2016-07-25 00:09:19 -0700

    Initial commit

---
+++ ResponseWebApp/ResponseWebApp/Scripts/General.js
@@ -0,0 +1,12 @@
+﻿$(function() {
+    $("a.mobile")
+        .click(function() {
+            $(".sidebar").slideToggle("fast");
+        });
+
+    $("#nav li a")
+        .click(function () {
+            $("#nav li a").removeClass("selected");
+            $(this).toggleClass("selected");
+        });
+});
